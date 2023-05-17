1a0aaa6537987834a2456a685ac2c7c2ef4b034d
Seth Woodson <sethwoodson3@gmail.com>
2023-05-16 21:03:34 -0500

    starting database

--- database.sql
+++ database.sql
@@ -3,8 +3,52 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
-CREATE TABLE "user" (
+CREATE TABLE 
+"user" (
+	"id" SERIAL PRIMARY KEY,
+	"username" VARCHAR (80) UNIQUE NOT NULL,
+	"password" VARCHAR (1000) NOT NULL
+);
+
+CREATE TABLE
+"dogs" (
+	"id" SERIAL PRIMARY KEY,
+	"user_id" integer,
+	"notes" VARCHAR (1000) NOT NULL,
+	"breed" VARCHAR (1000) NOT NULL,
+	"weight" DECIMAL,
+	"birthday" DATE,
+	"gender" VARCHAR (1000) NOT NULL
+);
+
+CREATE TABLE
+"dogs_food" (
     "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "dog_id" integer,
+    "food_id" integer,
+    "category_id" integer
+);
+
+CREATE TABLE
+"food" (
+    "id" SERIAL PRIMARY KEY,
+    "description" VARCHAR (1000) NOT NULL,
+    "favorite" BOOLEAN NOT NULL,
+    "notes" VARCHAR (1000) NOT NULL
+);
+
+CREATE TABLE
+"category" (
+    "id" SERIAL PRIMARY KEY,
+    "safe" BOOLEAN NOT NULL,
+    "allergy" BOOLEAN NOT NULL,
+    "name" VARCHAR (1000) NOT NULL
+);
+
+CREATE TABLE
+"poisonous_food" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR (1000) NOT NULL,
+    "symptoms" VARCHAR (1000) NOT NULL,
+    "notes" VARCHAR (1000) NOT NULL
 );
